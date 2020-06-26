192afa038709fbfa1f3e057d8c5504677a52decd
“Steve <stevedunnad@gmail.com>
2020-06-26 17:13:52 +0100

    formatted timeLeft

--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react"
+import momentDurationFormatSetup from 'moment-duration-format'
+import moment from 'moment'
+
+momentDurationFormatSetup(moment)
 
 function TimeLeft({ sessionLength }) {
   const [timeLeft, setTimeleft] = useState(sessionLength)
 
+  const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss')
+
   return (
     <>
-      <p>{timeLeft}</p>
+      <p>{formattedTimeLeft}</p>
     </>
   )
 }
