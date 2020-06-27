c135de51fd94b52ad948d90157838c21898ecd11
“Steve <stevedunnad@gmail.com>
2020-06-27 09:40:32 +0100

    start stop now works

--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -5,13 +5,38 @@ import moment from 'moment'
 momentDurationFormatSetup(moment)
 
 function TimeLeft({ sessionLength }) {
+  const [intervalId, setIntervalId] = useState(null)
   const [timeLeft, setTimeleft] = useState(sessionLength)
 
-  const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss')
+  useEffect(() => {
+    setTimeleft(sessionLength)
+  }, [sessionLength])
+
+  const isStarted = intervalId !== null
+  function handleStartStopClick() {
+    if (isStarted) {
+      clearInterval(intervalId)
+      setIntervalId(null)
+    } else {
+      const newIntervalId = setInterval(() => {
+        setTimeleft(prevTime => {
+          const newTimeLeft = prevTime - 1;
+          if (newTimeLeft >= 0) {
+            return prevTime - 1
+          }
+          return prevTime
+        })
+      }, 100)
+      setIntervalId(newIntervalId)
+    }
+  }
+
+  const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss', { trim: false })
 
   return (
     <>
       <p>{formattedTimeLeft}</p>
+      <button onClick={handleStartStopClick}>{isStarted ? 'Stop' : 'Start'}</button>
     </>
   )
 }
