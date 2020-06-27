dc86a080a33b66b7a30038baa3c27549d421d085
“Steve <stevedunnad@gmail.com>
2020-06-27 11:18:33 +0100

    moved timeLeft state up to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Break from './components/Break';
 import Session from './components/Session';
 import TimeLeft from './components/TimeLeft';
 
 function App() {
-
+  const audioElement = useRef(null)
+  const [currentSessionType, setCurrentSessionType] = useState('Session')
+  const [intervalId, setIntervalId] = useState(null)
   const [breakLength, setBreakLength] = useState(300)
+  const [sessionLength, setSessionLength] = useState(1500)
+  const [timeLeft, setTimeleft] = useState(sessionLength)
+
+  useEffect(() => {
+    setTimeleft(sessionLength)
+  }, [sessionLength])
 
   function decBreakByOneMin() {
     const newBreakLength = breakLength - 60;
@@ -22,8 +30,6 @@ function App() {
     setBreakLength(breakLength + 60)
   }
 
-  const [sessionLength, setSessionLength] = useState(300 * 5)
-
   function decSessionByOneMin() {
     const newSessionLength = sessionLength - 60;
 
@@ -38,8 +44,42 @@ function App() {
     setSessionLength(sessionLength + 60)
   }
 
-  function handleResetButtonClick() {
+  const isStarted = intervalId !== null
 
+  function handleStartStopClick() {
+    if (isStarted) {
+      clearInterval(intervalId)
+      setIntervalId(null)
+    } else {
+      const newIntervalId = setInterval(() => {
+        setTimeleft(prevTime => {
+          const newTimeLeft = prevTime - 1;
+          if (newTimeLeft >= 0) {
+            return prevTime - 1
+          }
+          audioElement.current.play()
+          if (currentSessionType === 'Session') {
+            setCurrentSessionType('Break')
+            setTimeleft(breakLength)
+          }
+          if (currentSessionType === 'Break') {
+            setCurrentSessionType('Session')
+            setTimeleft(sessionLength)
+          }
+        })
+      }, 100)
+      setIntervalId(newIntervalId)
+    }
+  }
+
+  function handleResetButtonClick() {
+    clearInterval(intervalId)
+    setIntervalId(null)
+    setSessionLength(1500)
+    setCurrentSessionType('Session')
+    setBreakLength(300)
+    setTimeleft(1500)
+    audioElement.current.load()
   }
 
   return (
@@ -49,12 +89,21 @@ function App() {
         decBreakByOneMin={decBreakByOneMin}
         incBreakByOneMin={incBreakByOneMin}
       />
-      <TimeLeft breakLength={breakLength} sessionLength={sessionLength} />
+      <TimeLeft breakLength={breakLength} timerLabel={currentSessionType} sessionLength={sessionLength}
+        handleStartStopClick={handleStartStopClick}
+        startStopButtonLabel={isStarted ? 'Stop' : 'Start'}
+        timeLeft={timeLeft}
+      />
       <Session
         sessionLength={sessionLength}
         decSessionByOneMin={decSessionByOneMin}
         incSessionByOneMin={incSessionByOneMin}
       />
+      <button id="reset" onClick={handleResetButtonClick} >
+        Reset</button>
+      <audio id='cuckoo' ref={audioElement}>
+        <source src="https://onlineclock.net/audio/options/cuckoo-clock.mp3" type="audio/mpeg" />
+      </audio>
     </div>
   )
 }
--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,51 +1,19 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import momentDurationFormatSetup from 'moment-duration-format'
 import moment from 'moment'
 
 momentDurationFormatSetup(moment)
 
-function TimeLeft({ sessionLength, breakLength }) {
-  const [currentSessionType, setCurrentSessionType] = useState('Session')
-  const [intervalId, setIntervalId] = useState(null)
-  const [timeLeft, setTimeleft] = useState(sessionLength)
-
-  useEffect(() => {
-    setTimeleft(sessionLength)
-  }, [sessionLength])
-
-  const isStarted = intervalId !== null
-  function handleStartStopClick() {
-    if (isStarted) {
-      clearInterval(intervalId)
-      setIntervalId(null)
-    } else {
-      const newIntervalId = setInterval(() => {
-        setTimeleft(prevTime => {
-          const newTimeLeft = prevTime - 1;
-          if (newTimeLeft >= 0) {
-            return prevTime - 1
-          }
-          if (currentSessionType === 'Session') {
-            setCurrentSessionType('Break')
-            setTimeleft(breakLength)
-          }
-          if (currentSessionType === 'Break') {
-            setCurrentSessionType('Session')
-            setTimeleft(sessionLength)
-          }
-        })
-      }, 100)
-      setIntervalId(newIntervalId)
-    }
-  }
-
+function TimeLeft({ timerLabel, handleStartStopClick, startStopButtonLabel, timeLeft }) {
   const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss', { trim: false })
 
   return (
     <>
-      <p id="timer-break">{currentSessionType}</p>
+      <p id="timer-break">{timerLabel}</p>
       <p id="time-left">{formattedTimeLeft}</p>
-      <button onClick={handleStartStopClick}>{isStarted ? 'Stop' : 'Start'}</button>
+      <button onClick={handleStartStopClick}>
+        {startStopButtonLabel}
+      </button>
     </>
   )
 }
